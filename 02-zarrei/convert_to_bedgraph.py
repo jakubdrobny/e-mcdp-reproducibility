da33f858ca9f275472c35f41b8da6405630c1367
jakubdrobny <jkubeing@gmail.com>
2025-05-19 15:33:00 +0200

    add bedgraphs for zarrei

--- 02-zarrei/convert_to_bedgraph.py
+++ 02-zarrei/convert_to_bedgraph.py
@@ -5,32 +5,37 @@ import argparse
 
 def tsv_to_bedgraph(input_dir):
     for cur_dir in os.listdir(input_dir):
-        input_path = os.path.join(input_dir, cur_dir, "output.tsv")
-        output_path = os.path.splitext(input_path)[0] + '.bedgraph'
-        df = pd.read_csv(input_path, sep='\t')
-        
-        df['data_value'] = df['p-value_adjusted'].apply(
-            lambda x: max(0.0, -math.log10(x)) if x > 0 else 0
-        )
-        
-        bedgraph_df = df[[
-            'chr_name', 
-            'begin', 
-            'end', 
-            'data_value'
-        ]].copy()
-        
-        bedgraph_df = bedgraph_df.sort_values(['chr_name', 'begin', 'end'])
-        
-        bedgraph_df.to_csv(
-            output_path,
-            sep='\t',
-            header=False,
-            index=False,
-            float_format='%.10f'
-        )
-        
-        print(f"Converted {input_path} to {output_path}")
+        for stati in ['z-score', 'p-value_adjusted']:
+            input_path = os.path.join(input_dir, cur_dir, "output.tsv")
+            output_path = os.path.splitext(input_path)[0].replace('output', stati) + '.bedgraph'
+            df = pd.read_csv(input_path, sep='\t')
+            
+            if stati != 'z-score':
+                df['data_value'] = df['p-value_adjusted'].apply(
+                    lambda x: max(0.0, -math.log10(x)) if x > 0 else 0
+                )
+            else:
+                df['end'] = df['end'].apply(lambda x: x - 900000 * (1 if cur_dir.endswith('1000000') else 11))
+                df['data_value'] = df['z-score'].fillna(0.0)
+            
+            bedgraph_df = df[[
+                'chr_name', 
+                'begin', 
+                'end', 
+                'data_value'
+            ]].copy()
+            
+            bedgraph_df = bedgraph_df.sort_values(['chr_name', 'begin', 'end'])
+            
+            bedgraph_df.to_csv(
+                output_path,
+                sep='\t',
+                header=False,
+                index=False,
+                float_format='%.10f'
+            )
+            
+            print(f"Converted {input_path} to {output_path}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
